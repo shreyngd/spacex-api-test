9dfc55041c47c18e3c6ad0513014566ead51a9ba
Naygandhi Shrey Sanjay <shrey.ns@external.swiggy.in>
2021-09-18 18:28:52 +0530

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.scss';
+import LaunchesListContainer from './components/LaunchesListContainer';
+import Stars from './components/Stars';
+import Rocket from './components/Rocket';
+import Header from './components/Header';
 
-const App = () : React.ReactElement =>  {
-  return (
-    <div className="App">
-        Hello Tesla
-    </div>
-  );
-}
+const App = (): React.ReactElement => {
+    return (
+        <div className="App">
+            <Stars />
+            <Rocket />
+            <Header />
+            <LaunchesListContainer />
+        </div>
+    );
+};
 
 export default App;
